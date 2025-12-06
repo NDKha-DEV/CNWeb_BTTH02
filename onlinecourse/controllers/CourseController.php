f3e16bc2f8070e27c64fb6b594e4293e41ef1e24
DucNguyen <ducnguyen2005105@gmail.com>
2025-12-06 14:45:28 +0700

    feat(course): block unauthorized access, check course ownership, and implement create functionality Course controller

---
+++ onlinecourse/controllers/CourseController.php
@@ -0,0 +1,47 @@
+<?php 
+require_once 'AuthController.php';
+require_once '../models/Course.php';
+require_once '../models/Category.php';
+require_once '../config/Database.php';
+
+class CourseController{
+    private $db;
+    private $course;
+    private $catergory;
+
+    private function __construct(){
+        $database = new Database();
+        $this->db = $database;
+        $this->course = new Course($this->db);
+        $this->catergory = new Category($this->db);
+    }
+
+    // Chặn truy cập nếu không có quyền truy cập
+    public function requirePermission(){
+        if(AuthController::isInstructorOrAdmin()){
+            echo "Bạn không có quyền truy cập chức năng này";
+            exit();
+        }
+    }
+
+    // Kiểm tra quyền sở hữu khóa học 
+    public function checkOwnership($courseIntructorId){
+        $currentUserId = AuthController::getCurrentUserId();
+        $currentUserRole = AuthController::getCurrentUserRole();
+
+        if($currentUserRole == 2) return true;
+        if($currentUserId == 1 && $currentUserId == $courseIntructorId) return true;
+
+        return false;
+    }
+
+    // Chức năng Create
+    public function create(){
+        $this->requirePermission();
+
+        $categories = $this->catergory->readAll();
+        require_once '../views/instructor/course/create.php';
+    }
+
+    
+}
